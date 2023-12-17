bd530b60d72e9404c2cb0de494d43589966ba52f
GaboChack <148814673+GaboChack@users.noreply.github.com>
2023-12-16 19:03:36 -0600

    Create Java.java

---
+++ Java/Java.java
@@ -0,0 +1,13 @@
+
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("SIUUUUUUUUU");
+    }
+
+}
+
+class HOLA {
+    public HOLA() {
+        System.out.println();
+    }
+}
